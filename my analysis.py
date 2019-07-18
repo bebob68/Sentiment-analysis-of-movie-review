b68ca55f8dd5197eeb5a6b39056de9c2de7e2bcd
Binesh Thoppil <bineshthoppils@gmail.com>
2019-07-18 13:50:19 +0530

    Add files via upload

---
+++ my analysis.py
@@ -0,0 +1,19 @@
+from numpy import *
+import matplotlib.pyplot as plt
+X_data = loadtxt("data/movie review/X_train.txt")
+print (X_data.shape)
+X = X_data
+y_data = loadtxt("data/movie review/y_train.txt", dtype = int)
+print (y_data.shape)
+y = y_data
+from sklearn.utils import shuffle
+X_new, y_new = shuffle(X, y)
+
+X_train = X_new[:1000]
+y_train = y_new[:1000]
+X_test = X_new[1000:]
+y_test = y_new[1000:]
+from sklearn.linear_model import LogisticRegression
+logreg = LogisticRegression()
+y_pred_test = logreg.predict(X_test)
+print ("Accuracy on test set:", logreg.score(X_test, y_test))
